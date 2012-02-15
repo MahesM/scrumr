96ac9aca0c4ccad7d38a78ec1d5992fbcfc3ff71
Chander S Pechetty <chander.p@imaginea.com>
2012-02-15 06:57:39 +0530

    removed sysouts, formatted and organized imports

--- src/main/java/com/imaginea/scrumr/services/TaskManagerImpl.java
+++ src/main/java/com/imaginea/scrumr/services/TaskManagerImpl.java
@@ -6,79 +6,78 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.imaginea.scrumr.entities.Story;
 import com.imaginea.scrumr.entities.Task;
 import com.imaginea.scrumr.interfaces.IDao;
 import com.imaginea.scrumr.interfaces.IEntity;
 import com.imaginea.scrumr.interfaces.TaskManager;
 
 public class TaskManagerImpl implements TaskManager {
-	
-	public static final Logger LOGGER = Logger.getLogger(TaskManagerImpl.class);
-	
-	private IDao<IEntity, Integer> genericDao;
-	
-	@Transactional
-	public void createTask(Task task) {
-		if(task != null) {
-			
-			genericDao.save(task);
-			
-		}
-	}
 
-	public Task readTask(Integer pkey) {
-		
-		return genericDao.find(Task.class, pkey);
-		
-	}
+    public static final Logger LOGGER = Logger.getLogger(TaskManagerImpl.class);
 
-	@Transactional
-	public void updateTask(Task task) {
-		if(task != null) {
+    private IDao<IEntity, Integer> genericDao;
 
-			genericDao.update(task);
-			
-		}
-	}
+    @Transactional
+    public void createTask(Task task) {
+        if (task != null) {
 
-	@Transactional
-	public void deleteTask(Task task) {
-		if(task != null) {
-			
-			genericDao.delete(task);
-			
-		}
-	}
-	
-	public List<Task> fetchTasksByStory(Integer pkey){
-		
-		//Story story = genericDao.find(Story.class, pkey);
-		Hashtable<String, Object> ht = new Hashtable<String, Object>();
-		ht.put("storyid", pkey);
-		System.out.println("--------------->>> Pkey of story:"+pkey);
-		List<Task>tasks =genericDao.getEntities(Task.class, "tasks.fetchTasksByStory",ht);
-		System.out.println("-------------------->>>>>> "+tasks);
-		return tasks;
-	}
-	
-	public List<Task> fetchTasksByStatus(String status){
-		
-		Hashtable<String, Object> ht = new Hashtable<String, Object>();
-		ht.put("status", status);
-		
-		return genericDao.getEntities(Task.class, "tasks.fetchTasksByStatus",ht);
-	}
-	
-	
-	/* Getters and Setters */
-	
-	public IDao<IEntity, Integer> getGenericDao() {
-		return genericDao;
-	}
+            genericDao.save(task);
 
-	public void setGenericDao(IDao<IEntity, Integer> genericDao) {
-		this.genericDao = genericDao;
-	}
+        }
+    }
+
+    public Task readTask(Integer pkey) {
+
+        return genericDao.find(Task.class, pkey);
+
+    }
+
+    @Transactional
+    public void updateTask(Task task) {
+        if (task != null) {
+
+            genericDao.update(task);
+
+        }
+    }
+
+    @Transactional
+    public void deleteTask(Task task) {
+        if (task != null) {
+
+            genericDao.delete(task);
+
+        }
+    }
+
+    public List<Task> fetchTasksByStory(Integer pkey) {
+
+        // Story story = genericDao.find(Story.class, pkey);
+        Hashtable<String, Object> ht = new Hashtable<String, Object>();
+        ht.put("storyid", pkey);
+        List<Task> tasks = genericDao.getEntities(Task.class, "tasks.fetchTasksByStory", ht);
+        if (LOGGER.isDebugEnabled())
+            LOGGER.debug("Tasks :" + tasks);
+
+        return tasks;
+    }
+
+    public List<Task> fetchTasksByStatus(String status) {
+
+        Hashtable<String, Object> ht = new Hashtable<String, Object>();
+        ht.put("status", status);
+
+        return genericDao.getEntities(Task.class, "tasks.fetchTasksByStatus", ht);
+    }
+
+    /* Getters and Setters */
+
+    public IDao<IEntity, Integer> getGenericDao() {
+        return genericDao;
+    }
+
+    public void setGenericDao(IDao<IEntity, Integer> genericDao) {
+        this.genericDao = genericDao;
+    }
 
 }
