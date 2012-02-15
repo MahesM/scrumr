c234510f14292544139b82f9341bee85f92f5e03
Chander S Pechetty <chander.p@imaginea.com>
2012-02-15 06:47:48 +0530

    formatted

--- src/main/java/com/imaginea/scrumr/entities/Task.java
+++ src/main/java/com/imaginea/scrumr/entities/Task.java
@@ -2,7 +2,6 @@ package com.imaginea.scrumr.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -15,69 +14,80 @@ import com.imaginea.scrumr.interfaces.IEntity;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name="tasks")
-@NamedQueries({
-	@NamedQuery(name="tasks.fetchTasksByStory", query="SELECT instance from Task instance where instance.story.id=:storyid" )
-})
+@Table(name = "tasks")
+@NamedQueries({ @NamedQuery(name = "tasks.fetchTasksByStory", query = "SELECT instance from Task instance where instance.story.id=:storyid") })
 @XmlRootElement
 public class Task extends AbstractEntity implements IEntity, Serializable {
 
-	private String content;
-	private String milestonePeriod;
-	private User user;
-	private Story story;
-	private String status;
+    private String content;
 
+    private String milestonePeriod;
 
-	@ManyToOne	
-	public User getUser() {
-		return user;
-	}
-	/**
-	 * @param user the user to set
-	 */
-	public void setUser(User user) {
-		this.user = user;
-	}
-	/**
-	 * @return the commentContent
-	 */
-	@Column(name="tcontent")
-	public String getContent() {
-		return content;
-	}
-	/**
-	 * @param commentContent the commentContent to set
-	 */
-	public void setContent(String content) {
-		this.content = content;
-	}
-	/**
-	 * @return the commentLogDate
-	 */
-	@Column(name="tmilestonemeriod")
-	public String getMilestonePeriod() {
-		return milestonePeriod;
-	}
-	/**
-	 * @param commentLogDate the commentLogDate to set
-	 */
-	public void setMilestonePeriod(String milestonePeriod) {
-		this.milestonePeriod = milestonePeriod;
-	}
-	/**
-	 * @return the story
-	 */
-	@ManyToOne
-	public Story getStory() {
-		return story;
-	}
-	/**
-	 * @param story the story to set
-	 */
-	public void setStory(Story story) {
-		this.story = story;
-	}
-	
-	
+    private User user;
+
+    private Story story;
+
+    private String status;
+
+    @ManyToOne
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * @param user
+     *            the user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the commentContent
+     */
+    @Column(name = "tcontent")
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param commentContent
+     *            the commentContent to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the commentLogDate
+     */
+    @Column(name = "tmilestonemeriod")
+    public String getMilestonePeriod() {
+        return milestonePeriod;
+    }
+
+    /**
+     * @param commentLogDate
+     *            the commentLogDate to set
+     */
+    public void setMilestonePeriod(String milestonePeriod) {
+        this.milestonePeriod = milestonePeriod;
+    }
+
+    /**
+     * @return the story
+     */
+    @ManyToOne
+    public Story getStory() {
+        return story;
+    }
+
+    /**
+     * @param story
+     *            the story to set
+     */
+    public void setStory(Story story) {
+        this.story = story;
+    }
+
 }
