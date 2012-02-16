d56cf6090b8c5d244c63b8f3f5891cdac66cd3c8
Chander S Pechetty <chander.p@imaginea.com>
2012-02-16 18:23:24 +0530

    formatted and organized imports

--- src/main/java/com/imaginea/scrumr/resources/StoryResource.java
+++ src/main/java/com/imaginea/scrumr/resources/StoryResource.java
@@ -3,8 +3,6 @@ package com.imaginea.scrumr.resources;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.Path;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,246 +25,243 @@ import com.imaginea.scrumr.interfaces.UserServiceManager;
 @RequestMapping("/stories")
 public class StoryResource {
 
-	@Autowired
-	ProjectManager projectManager;
+    @Autowired
+    ProjectManager projectManager;
 
-	@Autowired
-	SprintManager sprintManager;
+    @Autowired
+    SprintManager sprintManager;
 
-	@Autowired
-	UserServiceManager userServiceManager;
+    @Autowired
+    UserServiceManager userServiceManager;
 
-	@Autowired
-	StoryManager storyManager;
-	
-	@Autowired
-	StatusManager statusManager;
+    @Autowired
+    StoryManager storyManager;
 
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Story> fetchStory(@PathVariable("id") String id) {
+    @Autowired
+    StatusManager statusManager;
 
-		Story story = storyManager.readStory(Integer.parseInt(id));
-		List<Story> stories = new ArrayList<Story>();
-		stories.add(story);
-		return stories;
-	}
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Story> fetchStory(@PathVariable("id") String id) {
 
-	@RequestMapping(value="/project/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Story> fetchStoriesByProject(@PathVariable("id") String id) {
+        Story story = storyManager.readStory(Integer.parseInt(id));
+        List<Story> stories = new ArrayList<Story>();
+        stories.add(story);
+        return stories;
+    }
 
-		return storyManager.fetchStoriesByProject(Integer.parseInt(id));
+    @RequestMapping(value = "/project/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Story> fetchStoriesByProject(@PathVariable("id") String id) {
 
-	}
-	
-	@RequestMapping(value="{sprintid}/project/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Story> fetchStoriesByProjectSprint(@PathVariable("sprintid") String sid, @PathVariable("id") String pid) {
-		Sprint sprint = sprintManager.selectSprintByProject(projectManager.readProject(Integer.parseInt(pid)), Integer.parseInt(sid));
-		return storyManager.fetchStoriesBySprint(sprint.getPkey());
+        return storyManager.fetchStoriesByProject(Integer.parseInt(id));
 
-	}
+    }
 
-	@RequestMapping(value="/sprint/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Story> fetchStoriesBySprint(@PathVariable("id") String id) {
+    @RequestMapping(value = "{sprintid}/project/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Story> fetchStoriesByProjectSprint(@PathVariable("sprintid") String sid,
+                                    @PathVariable("id") String pid) {
+        Sprint sprint = sprintManager.selectSprintByProject(projectManager.readProject(Integer.parseInt(pid)), Integer.parseInt(sid));
+        return storyManager.fetchStoriesBySprint(sprint.getPkey());
 
-		return storyManager.fetchStoriesBySprint(Integer.parseInt(id));
+    }
 
-	}
+    @RequestMapping(value = "/sprint/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Story> fetchStoriesBySprint(@PathVariable("id") String id) {
 
-	@RequestMapping(value="/status", method = RequestMethod.POST)
-	public @ResponseBody List<Story> fetchStoriesByStatus(
-			@RequestParam String sprintid,
-			@RequestParam String status
-			) {
+        return storyManager.fetchStoriesBySprint(Integer.parseInt(id));
 
-		return storyManager.fetchStoriesByStatus(Integer.parseInt(sprintid),status);
+    }
 
-	}
+    @RequestMapping(value = "/status", method = RequestMethod.POST)
+    public @ResponseBody
+    List<Story> fetchStoriesByStatus(@RequestParam String sprintid, @RequestParam String status) {
 
-	@RequestMapping(value="/backlog/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Story> fetchUnAssignedStories(@PathVariable("id") String id) {
-		System.out.println("UnAssigned Stories");
-		return storyManager.fetchUnAssignedStories(Integer.parseInt(id));
+        return storyManager.fetchStoriesByStatus(Integer.parseInt(sprintid), status);
 
-	}
+    }
 
-	@RequestMapping(value="/create", method = RequestMethod.POST)
-	public @ResponseBody String createStory(
-			@RequestParam String stTitle,
-			@RequestParam String stDescription,
-			@RequestParam String stPriority,
-			@RequestParam String user,
-			@RequestParam String projectId,
-			@RequestParam String stSprint
-			) {
+    @RequestMapping(value = "/backlog/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Story> fetchUnAssignedStories(@PathVariable("id") String id) {
+        System.out.println("UnAssigned Stories");
+        return storyManager.fetchUnAssignedStories(Integer.parseInt(id));
 
-		Story story = new Story();
-		System.out.println("User :"+user);
-		try {
-			story.setTitle(stTitle);
-			story.setDescription(stDescription);
-			Sprint sprint = sprintManager.selectSprintByProject(projectManager.readProject(Integer.parseInt(projectId)),Integer.parseInt(stSprint));
-			story.setSprint_id(sprint);
-			story.setPriority(Integer.parseInt(stPriority));
-			story.setCreator(user);
-			story.setLast_updated(new java.sql.Date(System.currentTimeMillis()));
-			story.setLast_updatedby(user);
-			story.setCreation_date(new java.sql.Date(System.currentTimeMillis()));
-			story.setStatus("notstarted");
-			story.setView_count(0);
-			story.setProject(projectManager.readProject(Integer.parseInt(projectId)));
-			storyManager.createStory(story);
-		}catch(Exception e){
-			return "{\"result\":\"failure\"}";
-		}
-		return "{\"result\":\"success\"}";
-	}
+    }
 
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public @ResponseBody
+    String createStory(@RequestParam String stTitle, @RequestParam String stDescription,
+                                    @RequestParam String stPriority, @RequestParam String user,
+                                    @RequestParam String projectId, @RequestParam String stSprint) {
 
-	@RequestMapping(value="/addtosprint", method = RequestMethod.POST)
-	public @ResponseBody String addStoryToSprint(
-			@RequestParam String sprint,
-			@RequestParam String stories,
-			@RequestParam String status,
-			@RequestParam String projectId
-			) {
+        Story story = new Story();
+        System.out.println("User :" + user);
+        try {
+            story.setTitle(stTitle);
+            story.setDescription(stDescription);
+            Sprint sprint = sprintManager.selectSprintByProject(projectManager.readProject(Integer.parseInt(projectId)), Integer.parseInt(stSprint));
+            story.setSprint_id(sprint);
+            story.setPriority(Integer.parseInt(stPriority));
+            story.setCreator(user);
+            story.setLast_updated(new java.sql.Date(System.currentTimeMillis()));
+            story.setLast_updatedby(user);
+            story.setCreation_date(new java.sql.Date(System.currentTimeMillis()));
+            story.setStatus("notstarted");
+            story.setView_count(0);
+            story.setProject(projectManager.readProject(Integer.parseInt(projectId)));
+            storyManager.createStory(story);
+        } catch (Exception e) {
+            return "{\"result\":\"failure\"}";
+        }
+        return "{\"result\":\"success\"}";
+    }
 
-		try{
-			Story story = storyManager.readStory(Integer.parseInt(stories));
-			Sprint toSprint = sprintManager.selectSprintByProject(projectManager.readProject(Integer.parseInt(projectId)),Integer.parseInt(sprint));
-			story.setSprint_id(toSprint);
-			System.out.println(toSprint);
-			story.setStatus(status);
-			storyManager.updateStory(story);
-		}catch(Exception e){
-			return "{\"result\":\"failure\"}";
-		}
-		return "{\"result\":\"success\"}";
-	}
+    @RequestMapping(value = "/addtosprint", method = RequestMethod.POST)
+    public @ResponseBody
+    String addStoryToSprint(@RequestParam String sprint, @RequestParam String stories,
+                                    @RequestParam String status, @RequestParam String projectId) {
 
-	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
-	public @ResponseBody String deleteStory(@PathVariable("id") String id) {
+        try {
+            Story story = storyManager.readStory(Integer.parseInt(stories));
+            Sprint toSprint = sprintManager.selectSprintByProject(projectManager.readProject(Integer.parseInt(projectId)), Integer.parseInt(sprint));
+            story.setSprint_id(toSprint);
+            System.out.println(toSprint);
+            story.setStatus(status);
+            storyManager.updateStory(story);
+        } catch (Exception e) {
+            return "{\"result\":\"failure\"}";
+        }
+        return "{\"result\":\"success\"}";
+    }
 
-		Story story = storyManager.readStory(Integer.parseInt(id));
-		System.out.println("Title: "+story.getTitle());
-		storyManager.deleteStory(story);
-		return "{\"result\":\"success\"}";
-	}
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    String deleteStory(@PathVariable("id") String id) {
 
-	@RequestMapping(value="/{id}/adduser/{uid}", method = RequestMethod.GET)
-	public @ResponseBody String addUserToStory(@PathVariable("id") String id, @PathVariable("uid") String uid) {
+        Story story = storyManager.readStory(Integer.parseInt(id));
+        System.out.println("Title: " + story.getTitle());
+        storyManager.deleteStory(story);
+        return "{\"result\":\"success\"}";
+    }
 
-		User user = userServiceManager.readUser(id);
-		Story story = storyManager.readStory(Integer.parseInt(id));
-		story.addAssignees(user);
-		storyManager.updateStory(story);
-		return "{\"result\":\"success\"}";
-	}
+    @RequestMapping(value = "/{id}/adduser/{uid}", method = RequestMethod.GET)
+    public @ResponseBody
+    String addUserToStory(@PathVariable("id") String id, @PathVariable("uid") String uid) {
 
-	@RequestMapping(value="/{id}/remove/{uid}", method = RequestMethod.POST)
-	public @ResponseBody String removeUserFromStory(@RequestParam("id") String id, @RequestParam("uid") String uid) {
+        User user = userServiceManager.readUser(id);
+        Story story = storyManager.readStory(Integer.parseInt(id));
+        story.addAssignees(user);
+        storyManager.updateStory(story);
+        return "{\"result\":\"success\"}";
+    }
 
-		User user = userServiceManager.readUser(id);
-		Story story = storyManager.readStory(Integer.parseInt(id));
-		story.removeAssignees(user);
-		storyManager.updateStory(story);
-		return "{\"result\":\"success\"}";
-	}
-	
-	@RequestMapping(value="removeuser", method = RequestMethod.POST)
-	public @ResponseBody String removeUserFromStage(@RequestParam("userid") String uid, @RequestParam("storyId") String stid, @RequestParam("stageId") String stage ) {
+    @RequestMapping(value = "/{id}/remove/{uid}", method = RequestMethod.POST)
+    public @ResponseBody
+    String removeUserFromStory(@RequestParam("id") String id, @RequestParam("uid") String uid) {
 
-		Status status = statusManager.fetchUserStoryStatus(Integer.parseInt(stid), stage, uid);
-		statusManager.deleteStatus(status);
-		return "{\"result\":\"success\"}";
-	}
+        User user = userServiceManager.readUser(id);
+        Story story = storyManager.readStory(Integer.parseInt(id));
+        story.removeAssignees(user);
+        storyManager.updateStory(story);
+        return "{\"result\":\"success\"}";
+    }
 
-	@RequestMapping(value="/{id}/addusers/{uids}", method = RequestMethod.GET)
-	public @ResponseBody String addUsersToStory(@PathVariable("id") String id, @PathVariable("uids") String uids) {
+    @RequestMapping(value = "removeuser", method = RequestMethod.POST)
+    public @ResponseBody
+    String removeUserFromStage(@RequestParam("userid") String uid,
+                                    @RequestParam("storyId") String stid,
+                                    @RequestParam("stageId") String stage) {
 
-		Story story = storyManager.readStory(Integer.parseInt(id));
-		String[] users = uids.split(",");
-		for(String s: users){
-			User user = userServiceManager.readUser(s);
-			story.addAssignees(user);
-		}
-		storyManager.updateStory(story);
-		return "{\"result\":\"success\"}";
-	}
+        Status status = statusManager.fetchUserStoryStatus(Integer.parseInt(stid), stage, uid);
+        statusManager.deleteStatus(status);
+        return "{\"result\":\"success\"}";
+    }
 
-	@RequestMapping(value="/{id}/removeusers/{uidlist}", method = RequestMethod.GET)
-	public @ResponseBody String removeUsersFromStory(@PathVariable("id") String id, @PathVariable("uids") String uids) {
+    @RequestMapping(value = "/{id}/addusers/{uids}", method = RequestMethod.GET)
+    public @ResponseBody
+    String addUsersToStory(@PathVariable("id") String id, @PathVariable("uids") String uids) {
 
-		Story story = storyManager.readStory(Integer.parseInt(id));
-		String[] users = uids.split(",");
-		for(String s: users){
-			User user = userServiceManager.readUser(s);
-			story.removeAssignees(user);
-		}
-		storyManager.updateStory(story);
-		return "{\"result\":\"success\"}";
-	}
-	
-	@RequestMapping(value="/adduserwithstage", method = RequestMethod.POST)
-	public @ResponseBody String addUserWithStage(
-			@RequestParam String userid,
-			@RequestParam String storyId,
-			@RequestParam String stage
-		) throws Exception {
-		try{
-		Status story_status = new Status();
-		story_status.setUser(userServiceManager.readUser(userid));
-		story_status.setStory(storyManager.readStory(Integer.parseInt(storyId)));
-		story_status.setStage(stage);
-		System.out.println("Stage :"+stage+", User :"+userid+", story"+storyId);
-		statusManager.createStatus(story_status);
-		}catch(Exception e){
-			throw new Exception(e.toString());
-		}
-		return "{\"result\":\"success\"}";		
-	}
-	
-	@RequestMapping(value="/getusers", method = RequestMethod.POST)
-	public @ResponseBody List<Status> getUsersFromStory(
-			@RequestParam String storyId,
-			@RequestParam String stage
-		) {
-		
-		return statusManager.fetchStoryStatus(Integer.parseInt(storyId), stage);
-	}
-	
-	@RequestMapping(value="/adduserswithstage", method = RequestMethod.POST)
-	public @ResponseBody String addUsersWithStage(
-			@RequestParam String userids,
-			@RequestParam String storyId,
-			@RequestParam String stage
-		) {
-		boolean isFailed = false;
-		String exceptionString = "";
-		String[] users = userids.split(",");
-		for(String s: users){
-			try{
-			addUserWithStage(s,storyId,stage);
-			}catch(Exception e){
-				isFailed = true;
-				exceptionString += s+",";
-			}
-		}
-		if (isFailed){
-			//System.out.println("string :"+exceptionString.substring(0,exceptionString.length()-1));
-			exceptionString = exceptionString.substring(0,exceptionString.length()-1);			
-			return "{\"result\":\"failure: Exception occured while adding some users. Users :"+exceptionString+"\"}";
-		}
-		return "{\"result\":\"success\"}";		
-		//return "{\"result\":\"failure\"}";
-	}
-	
-	@RequestMapping(value="/clearstoryassignees", method = RequestMethod.POST)
-	public @ResponseBody String removeUsersWithStage(
-			@RequestParam String storyId,
-			@RequestParam String stage
-		) {
-		System.out.println("In Story Resource clear:story id ="+storyId+", stage ="+stage);
-		statusManager.clearUsersByStage(Integer.parseInt(storyId), stage);
-		return "{\"result\":\"success\"}";
-	}
+        Story story = storyManager.readStory(Integer.parseInt(id));
+        String[] users = uids.split(",");
+        for (String s : users) {
+            User user = userServiceManager.readUser(s);
+            story.addAssignees(user);
+        }
+        storyManager.updateStory(story);
+        return "{\"result\":\"success\"}";
+    }
+
+    @RequestMapping(value = "/{id}/removeusers/{uidlist}", method = RequestMethod.GET)
+    public @ResponseBody
+    String removeUsersFromStory(@PathVariable("id") String id, @PathVariable("uids") String uids) {
+
+        Story story = storyManager.readStory(Integer.parseInt(id));
+        String[] users = uids.split(",");
+        for (String s : users) {
+            User user = userServiceManager.readUser(s);
+            story.removeAssignees(user);
+        }
+        storyManager.updateStory(story);
+        return "{\"result\":\"success\"}";
+    }
+
+    @RequestMapping(value = "/adduserwithstage", method = RequestMethod.POST)
+    public @ResponseBody
+    String addUserWithStage(@RequestParam String userid, @RequestParam String storyId,
+                                    @RequestParam String stage) throws Exception {
+        try {
+            Status story_status = new Status();
+            story_status.setUser(userServiceManager.readUser(userid));
+            story_status.setStory(storyManager.readStory(Integer.parseInt(storyId)));
+            story_status.setStage(stage);
+            System.out.println("Stage :" + stage + ", User :" + userid + ", story" + storyId);
+            statusManager.createStatus(story_status);
+        } catch (Exception e) {
+            throw new Exception(e.toString());
+        }
+        return "{\"result\":\"success\"}";
+    }
+
+    @RequestMapping(value = "/getusers", method = RequestMethod.POST)
+    public @ResponseBody
+    List<Status> getUsersFromStory(@RequestParam String storyId, @RequestParam String stage) {
+
+        return statusManager.fetchStoryStatus(Integer.parseInt(storyId), stage);
+    }
+
+    @RequestMapping(value = "/adduserswithstage", method = RequestMethod.POST)
+    public @ResponseBody
+    String addUsersWithStage(@RequestParam String userids, @RequestParam String storyId,
+                                    @RequestParam String stage) {
+        boolean isFailed = false;
+        String exceptionString = "";
+        String[] users = userids.split(",");
+        for (String s : users) {
+            try {
+                addUserWithStage(s, storyId, stage);
+            } catch (Exception e) {
+                isFailed = true;
+                exceptionString += s + ",";
+            }
+        }
+        if (isFailed) {
+            // System.out.println("string :"+exceptionString.substring(0,exceptionString.length()-1));
+            exceptionString = exceptionString.substring(0, exceptionString.length() - 1);
+            return "{\"result\":\"failure: Exception occured while adding some users. Users :"
+                                            + exceptionString + "\"}";
+        }
+        return "{\"result\":\"success\"}";
+        // return "{\"result\":\"failure\"}";
+    }
+
+    @RequestMapping(value = "/clearstoryassignees", method = RequestMethod.POST)
+    public @ResponseBody
+    String removeUsersWithStage(@RequestParam String storyId, @RequestParam String stage) {
+        System.out.println("In Story Resource clear:story id =" + storyId + ", stage =" + stage);
+        statusManager.clearUsersByStage(Integer.parseInt(storyId), stage);
+        return "{\"result\":\"success\"}";
+    }
 }
