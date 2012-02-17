9d12076e8064c3825b681bc5f4bf0cfa05782316
Chander S Pechetty <chander.p@imaginea.com>
2012-02-17 19:56:59 +0530

    added update sprint and projectId to the sprint create call
    added update sprint call and for add call-retrieve the project before
    creating the sprint. Formatted.

--- src/main/java/com/imaginea/scrumr/resources/SprintResource.java
+++ src/main/java/com/imaginea/scrumr/resources/SprintResource.java
@@ -23,68 +23,99 @@ import com.imaginea.scrumr.interfaces.UserServiceManager;
 @RequestMapping("/sprints")
 public class SprintResource {
 
-	@Autowired
-	ProjectManager projectManager;
+    @Autowired
+    ProjectManager projectManager;
 
-	@Autowired
-	SprintManager sprintManager;
+    @Autowired
+    SprintManager sprintManager;
 
-	@Autowired
-	UserServiceManager userServiceManager;
+    @Autowired
+    UserServiceManager userServiceManager;
 
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Sprint> fetchSprint(@PathVariable("id") String id) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Sprint> fetchSprint(@PathVariable("id") String id) {
 
-		Sprint sprint = sprintManager.readSprint(Integer.parseInt(id));
-		List<Sprint> sprints = new ArrayList<Sprint>();
-		sprints.add(sprint);
-		return sprints;
-	}
+        Sprint sprint = sprintManager.readSprint(Integer.parseInt(id));
+        List<Sprint> sprints = new ArrayList<Sprint>();
+        sprints.add(sprint);
+        return sprints;
+    }
 
-	@RequestMapping(value="{sprintid}/project/{projectid}", method = RequestMethod.GET)
-	public @ResponseBody List<Sprint> fetchSprintByProject(@PathVariable("sprintid") String id, @PathVariable("projectid") String pid) {
+    @RequestMapping(value = "{sprintid}/project/{projectid}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Sprint> fetchSprintByProject(@PathVariable("sprintid") String id,
+                                    @PathVariable("projectid") String pid) {
 
-		Project project = projectManager.readProject(Integer.parseInt(pid));
-		Sprint sprint = sprintManager.selectSprintByProject(project,Integer.parseInt(id));
-		List<Sprint> sprints = new ArrayList<Sprint>();
-		sprints.add(sprint);
-		return sprints;
-	}
+        Project project = projectManager.readProject(Integer.parseInt(pid));
+        Sprint sprint = sprintManager.selectSprintByProject(project, Integer.parseInt(id));
+        List<Sprint> sprints = new ArrayList<Sprint>();
+        sprints.add(sprint);
+        return sprints;
+    }
 
-	@RequestMapping(value="/project/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Sprint> fetchSprintsByProject(@PathVariable("id") String id) {
+    @RequestMapping(value = "/project/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Sprint> fetchSprintsByProject(@PathVariable("id") String id) {
 
-		Project project = projectManager.readProject(Integer.parseInt(id));
-		List<Sprint> sprints = sprintManager.selectSprintsByProject(project);
-		return sprints;
-	}
+        Project project = projectManager.readProject(Integer.parseInt(id));
+        List<Sprint> sprints = sprintManager.selectSprintsByProject(project);
+        return sprints;
+    }
 
-	@RequestMapping(value="/create", method = RequestMethod.POST)
-	public @ResponseBody String createSprint(
-			@RequestParam String start_date,
-			@RequestParam String end_date
-			) {
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public @ResponseBody
+    String createSprint(@RequestParam String start_date, @RequestParam String end_date,
+                                    @RequestParam String projectId) {
 
-		Sprint sprint = new Sprint();
-		try {
-			SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-			Date startdate = format.parse(start_date);
-			sprint.setStartdate(startdate);
-			Date enddate = format.parse(start_date);
-			sprint.setEnddate(enddate);
-			if(startdate.after(new Date())){
-				sprint.setStatus("Not Started");
-			}else{
-				if(enddate.before(new Date())){
-					sprint.setStatus("Finished");
-				}else{
-					sprint.setStatus("In Progress");
-				}
-			}
-			sprintManager.createSprint(sprint);
-		}catch(Exception e){
-			return "{\"result\":\"failure\"}";
-		}
-		return "{\"result\":\"success\"}";
-	}
+        Sprint sprint = new Sprint();
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+            Date startdate = format.parse(start_date);
+            sprint.setStartdate(startdate);
+            Date enddate = format.parse(start_date);
+            sprint.setEnddate(enddate);
+            if (startdate.after(new Date())) {
+                sprint.setStatus("Not Started");
+            } else {
+                if (enddate.before(new Date())) {
+                    sprint.setStatus("Finished");
+                } else {
+                    sprint.setStatus("In Progress");
+                }
+            }
+            Project project = projectManager.readProject(Integer.parseInt(projectId));
+            sprint.setProject(project);
+            sprintManager.createSprint(sprint);
+        } catch (Exception e) {
+            return "{\"result\":\"failure\"}";
+        }
+        return "{\"result\":\"success\"}";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public @ResponseBody
+    String updateSprint(@RequestParam String start_date, @RequestParam String end_date,
+                                    @RequestParam String sprintId) {
+        Sprint sprint = sprintManager.readSprint(Integer.parseInt(sprintId));
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+            Date startdate = format.parse(start_date);
+            sprint.setStartdate(startdate);
+            Date enddate = format.parse(start_date);
+            sprint.setEnddate(enddate);
+            if (startdate.after(new Date())) {
+                sprint.setStatus("Not Started");
+            } else {
+                if (enddate.before(new Date())) {
+                    sprint.setStatus("Finished");
+                } else {
+                    sprint.setStatus("In Progress");
+                }
+            }
+        } catch (Exception e) {
+            return "{\"result\":\"failure\"}";
+        }
+        return "{\"result\":\"success\"}";
+    }
 }
