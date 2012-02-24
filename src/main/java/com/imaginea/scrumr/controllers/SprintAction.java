a82116b00957e521ef1647744f0eea113743c6d5
Chander S Pechetty <chander.p@imaginea.com>
2012-02-24 19:47:21 +0530

    added null check for source and formatted

--- src/main/java/com/imaginea/scrumr/controllers/SprintAction.java
+++ src/main/java/com/imaginea/scrumr/controllers/SprintAction.java
@@ -1,73 +1,71 @@
 package com.imaginea.scrumr.controllers;
 
-import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
-
-import com.imaginea.scrumr.qontextclient.QontextRestApiInvocationUtil;
 
 @SuppressWarnings("serial")
 public class SprintAction extends GenericActionSupport {
 
+    public JSONObject successResponse = null;
 
-	public JSONObject successResponse = null;
-	private String qontextHostUrl;
-	private String storyDescLimit;
-	private String storyTitleLimit;
-	public String source;
-	
-	@Override
-	public void prepare() throws Exception {
-		super.prepare();
-	}
+    private String qontextHostUrl;
 
-	public String prepareSprintHome() throws Exception {
-		if(loggedInUser != null){
-			source = (String) request.getSession().getAttribute("source");
-			System.out.println("Source:"+source);
-			if(source.equalsIgnoreCase("facebook")){
-				qontextHostUrl = "http://graph.facebook.com";
-				System.out.println("Source:"+qontextHostUrl);
-			}else if(source.equalsIgnoreCase("google")){
-				qontextHostUrl = "";
-				System.out.println("Source:"+qontextHostUrl);
-			}
-			return SUCCESS;
-		}else{
-			return ERROR;
-		}
-	}
+    private String storyDescLimit;
 
-	public String prepareSprintStaticPage() {
+    private String storyTitleLimit;
 
-		return SUCCESS;
-	}
+    public String source;
 
-	public String getQontextHostUrl() {
-		return qontextHostUrl;
-	}
+    @Override
+    public void prepare() throws Exception {
+        super.prepare();
+    }
 
-	public void setQontextHostUrl(String qontextHostUrl) {
-		this.qontextHostUrl = qontextHostUrl;
-	}
+    public String prepareSprintHome() throws Exception {
+        if (loggedInUser != null) {
+            source = (String) request.getSession().getAttribute("source");
+            System.out.println("Source:" + source);
+            if (source != null) {
+                if (source.equalsIgnoreCase("facebook")) {
+                    qontextHostUrl = "http://graph.facebook.com";
+                    System.out.println("Source:" + qontextHostUrl);
+                } else if (source.equalsIgnoreCase("google")) {
+                    qontextHostUrl = "";
+                    System.out.println("Source:" + qontextHostUrl);
+                }
+            }
+            return SUCCESS;
+        } else {
+            return ERROR;
+        }
+    }
 
-	public String getStoryDescLimit() {
-		return storyDescLimit;
-	}
+    public String prepareSprintStaticPage() {
 
-	public void setStoryDescLimit(String storyDescLimit) {
-		this.storyDescLimit = storyDescLimit;
-	}
+        return SUCCESS;
+    }
 
-	public String getStoryTitleLimit() {
-		return storyTitleLimit;
-	}
+    public String getQontextHostUrl() {
+        return qontextHostUrl;
+    }
 
-	public void setStoryTitleLimit(String storyTitleLimit) {
-		this.storyTitleLimit = storyTitleLimit;
-	}
-	
-	
-	
-	
+    public void setQontextHostUrl(String qontextHostUrl) {
+        this.qontextHostUrl = qontextHostUrl;
+    }
+
+    public String getStoryDescLimit() {
+        return storyDescLimit;
+    }
+
+    public void setStoryDescLimit(String storyDescLimit) {
+        this.storyDescLimit = storyDescLimit;
+    }
+
+    public String getStoryTitleLimit() {
+        return storyTitleLimit;
+    }
+
+    public void setStoryTitleLimit(String storyTitleLimit) {
+        this.storyTitleLimit = storyTitleLimit;
+    }
 
 }
