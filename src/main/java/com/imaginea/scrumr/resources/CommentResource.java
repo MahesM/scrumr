8a54636fc91df2c947721bf59f3b9f29f3465eb5
Chander S Pechetty <chander.p@imaginea.com>
2012-02-24 16:42:57 +0530

    formatted and organized imports

--- src/main/java/com/imaginea/scrumr/resources/CommentResource.java
+++ src/main/java/com/imaginea/scrumr/resources/CommentResource.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.ws.rs.Path;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,73 +25,73 @@ import com.imaginea.scrumr.interfaces.UserServiceManager;
 @RequestMapping("/comments")
 public class CommentResource {
 
-	@Autowired
-	ProjectManager projectManager;
+    @Autowired
+    ProjectManager projectManager;
 
-	@Autowired
-	CommentManager commentManager;
+    @Autowired
+    CommentManager commentManager;
 
-	@Autowired
-	StoryManager storyManager;
+    @Autowired
+    StoryManager storyManager;
 
-	@Autowired
-	UserServiceManager userServiceManager;
+    @Autowired
+    UserServiceManager userServiceManager;
 
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Comment> fetchComment(@PathVariable("id") String id) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Comment> fetchComment(@PathVariable("id") String id) {
 
-		Comment comment = commentManager.readComment(Integer.parseInt(id));
-		List<Comment> comments = new ArrayList<Comment>();
-		comments.add(comment);
-		return comments;
-	}
-	
-	@RequestMapping(value="/story/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Comment> fetchCommentsByStory(@PathVariable("id") String id) {
+        Comment comment = commentManager.readComment(Integer.parseInt(id));
+        List<Comment> comments = new ArrayList<Comment>();
+        comments.add(comment);
+        return comments;
+    }
 
-		return commentManager.fetchCommentsByStory(Integer.parseInt(id));
-	}
+    @RequestMapping(value = "/story/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Comment> fetchCommentsByStory(@PathVariable("id") String id) {
 
-	@RequestMapping(value="/create", method = RequestMethod.POST)
-	public @ResponseBody List<Comment> createSprint(
-			@RequestParam String logdate,
-			@RequestParam String user,
-			@RequestParam String content,
-			@RequestParam String storyid
-			) {
+        return commentManager.fetchCommentsByStory(Integer.parseInt(id));
+    }
 
-		Comment comment = new Comment();
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public @ResponseBody
+    List<Comment> createSprint(@RequestParam String logdate, @RequestParam String user,
+                                    @RequestParam String content, @RequestParam String storyid) {
 
-		DateFormat dateFormat = new SimpleDateFormat("yy-mm-dd");
-		Date logDate=null;		
+        Comment comment = new Comment();
 
-		try {
+        DateFormat dateFormat = new SimpleDateFormat("yy-mm-dd");
+        Date logDate = null;
 
-			logDate = dateFormat.parse(logdate);
-			comment.setContent(content);
-			comment.setLogDate(logDate);
-			comment.setUser(userServiceManager.readUser(user));
-			comment.setStory(storyManager.readStory(Integer.parseInt(storyid)));
-			
-			commentManager.createComment(comment);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return null;
-		}
+        try {
 
-		List<Comment> result = new ArrayList<Comment>();
-		result.add(comment);
-		return result;
+            logDate = dateFormat.parse(logdate);
+            comment.setContent(content);
+            comment.setLogDate(logDate);
+            comment.setUser(userServiceManager.readUser(user));
+            comment.setStory(storyManager.readStory(Integer.parseInt(storyid)));
 
-	}
+            commentManager.createComment(comment);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
 
-	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
-	public @ResponseBody String deleteComment(@PathVariable("id") String id) {
-		Comment comment = commentManager.readComment(Integer.parseInt(id));
-		System.out.println("Comment : "+comment.getContent());		
-		commentManager.deleteComment(comment);
-		System.out.println("Comment deleted");
-		return "{\"result\":\"success\"}";
-	}
+        List<Comment> result = new ArrayList<Comment>();
+        result.add(comment);
+        return result;
+
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    String deleteComment(@PathVariable("id") String id) {
+        Comment comment = commentManager.readComment(Integer.parseInt(id));
+        System.out.println("Comment : " + comment.getContent());
+        commentManager.deleteComment(comment);
+        System.out.println("Comment deleted");
+        return "{\"result\":\"success\"}";
+    }
 
 }
--- src/main/java/com/imaginea/scrumr/resources/ProjectResource.java
+++ src/main/java/com/imaginea/scrumr/resources/ProjectResource.java
@@ -1,6 +1,5 @@
 package com.imaginea.scrumr.resources;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,9 +8,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,269 +27,263 @@ import com.imaginea.scrumr.interfaces.UserServiceManager;
 @Controller
 @RequestMapping("/projects")
 public class ProjectResource {
-	
-	@Autowired
-	ProjectManager projectManager;
-	
-	@Autowired
-	SprintManager sprintManager;
-	
-	@Autowired
-	UserServiceManager userServiceManager;
 
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public @ResponseBody List<Project> fetchProject(@PathVariable("id") String id) {
-		Project project = projectManager.readProject(Integer.parseInt(id));
-		List<Project> projects = new ArrayList<Project>();
-		projects.add(project);
-		return projects;
-	}
-	
-	@RequestMapping(value="/user/{userid}", method = RequestMethod.GET)
-	public @ResponseBody List<Project> fetchProjectsByUser(@PathVariable("userid") String id) {
-		User user = userServiceManager.readUser(id);
-		if(user != null){
-			return user.getProjects();
-		}else{
-			return null;
-		}
-			
-	}
-	
-	@RequestMapping(value="/adduser", method = RequestMethod.POST)
-	public @ResponseBody String addUser(
-			@RequestParam String userid,
-    		@RequestParam String projectId
-		) {
-		String uid = userid;
-		System.out.println("User Id :"+uid);
-		Integer pid = Integer.parseInt(projectId);
-		Project project = projectManager.readProject(pid);
-		System.out.println("User Object :"+userServiceManager.readUser(uid));
-		project.addAssignees(userServiceManager.readUser(uid));
-		projectManager.updateProject(project);
-		return "{\"result\":\"success\"}";
-	}
-	
-	@RequestMapping(value="/removeuser", method = RequestMethod.POST)
-	public @ResponseBody String removeUser(
-			@RequestParam String userid,
-    		@RequestParam String projectId
-		) {
-		String uid = userid;
-		Integer pid = Integer.parseInt(projectId);
-		Project project = projectManager.readProject(pid);
-		project.removeAssignees(userServiceManager.readUser(uid));
-		projectManager.updateProject(project);
-		return "{\"result\":\"success\"}";
-	}
-	
-	@RequestMapping(value="/storycount/{id}", method = RequestMethod.GET)
-	public @ResponseBody String projectCounts(@PathVariable("id") String id) {
-		Integer pid = Integer.parseInt(id);
-		Project project = projectManager.readProject(pid);
-		Set<Story> stories = (Set<Story>)project.getStories();
-		if(stories != null){
-			return "{\"result\":"+stories.size()+"}";
-		}
-		return "{\"result\":\"failure\"}";
-		
-	}
-	
-	@RequestMapping(value="/create", method = RequestMethod.POST)	
-    public @ResponseBody List<Project> createProject(
-    		@RequestParam String pTitle,
-    		@RequestParam String pDescription,
-    		@RequestParam String pSprintDuration,
-    		@RequestParam String pStartDate,
-    		@RequestParam String pEndDate,
-    		@RequestParam String assignees,
-    		@RequestParam String current_user
-    		)
-	
-	{
-		Project project = new Project();	
-		try {
-			
-			project.setTitle(pTitle);
-			project.setDescription(pDescription);
-			Date date = null;
-			Date date1 = null;
-			int sprint_count =0;
-			int duration = Integer.parseInt(pSprintDuration);
-			try {
-				SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-				if(pStartDate != "" && pEndDate != ""){
-					date = format.parse(pStartDate);
-					date1 = format.parse(pEndDate);
-					project.setStart_date(date);
-					project.setEnd_date(date1);
-					sprint_count  = getSprintCount(date, date1,Integer.parseInt(pSprintDuration));
-					project.setNo_of_sprints(sprint_count);
-				}else{
-					date = format.parse(pStartDate);
-					project.setStart_date(date);
-					project.setCurrent_sprint(0);
-					project.setEnd_date(null);
-					sprint_count  = getSprintCount(date, null,Integer.parseInt(pSprintDuration));
-					System.out.println("Test: SC: "+sprint_count);
-					project.setNo_of_sprints(sprint_count);
-				}
-			} catch (ParseException e1) {
-				e1.printStackTrace();
-			}
-			String userStr = assignees;
-			String[] users = userStr.split(",");
-			Set<User> userList =  new HashSet<User>();
-			for(String s: users){
-				System.out.println("user: "+s);
-				User user = userServiceManager.readUser(s);
-				userList.add(user);
-			}
+    @Autowired
+    ProjectManager projectManager;
 
-			if(date.after(new Date())){
-				project.setCurrent_sprint(0);
-				project.setStatus("Not Started");
-			}else{
-				project.setCurrent_sprint(getCurrentSprint(date,date1,duration));
-				project.setStatus("In Progress");
-			}
-			
-			project.setAssignees(userList);
-			project.setSprint_duration(duration);
-			project.setCreatedby(current_user);
-			project.setLast_updated(new java.sql.Date(System.currentTimeMillis()));
-			project.setLast_updatedby(current_user);
-			project.setCreation_date(new java.sql.Date(System.currentTimeMillis()));
+    @Autowired
+    SprintManager sprintManager;
 
-			projectManager.createProject(project);
-			Date currentdate = date;
-			for(int i=0;i<sprint_count;i++){
+    @Autowired
+    UserServiceManager userServiceManager;
 
-				Sprint sprint = new Sprint();
-				sprint.setId(i+1);
-				sprint.setStartdate(new java.sql.Date(currentdate.getTime()));
-				Date enddate = new Date(currentdate.getTime() + ((7*duration)*86400000L) - 3600000L);
-				if(date1 !=  null && i == (sprint_count-1)){
-					enddate = new Date(currentdate.getTime() + ((7*duration)*86400000L));
-				}
-				if(date1 != null){
-					if(enddate.before(date1)){
-						sprint.setEnddate(new java.sql.Date(enddate.getTime()));
-					}else{
-						sprint.setEnddate(new java.sql.Date(date1.getTime() + (86400000L - 3600000L) ));
-					}
-				}else{
-					sprint.setEnddate(new java.sql.Date(enddate.getTime()));
-				}
-				if(currentdate.after(new Date())){
-        			sprint.setStatus("Not Started");
-        		}else{
-        			if(enddate.before(new Date())){
-        				System.out.println(enddate.toString());
-            			sprint.setStatus("Finished");
-            		}else{
-            			System.out.println("Past sprint");
-            			sprint.setStatus("In Progress");
-            			project.setCurrent_sprint(sprint.getId());
-            			project.setStatus("In Progress");
-            			projectManager.updateProject(project);
-            		}
-        		}
-				sprint.setProject(project);
-				sprintManager.createSprint(sprint);
-				currentdate = new Date(currentdate.getTime() + ((7*duration)*86400000L));
-			}
-			
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Project> fetchProject(@PathVariable("id") String id) {
+        Project project = projectManager.readProject(Integer.parseInt(id));
+        List<Project> projects = new ArrayList<Project>();
+        projects.add(project);
+        return projects;
+    }
+
+    @RequestMapping(value = "/user/{userid}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Project> fetchProjectsByUser(@PathVariable("userid") String id) {
+        User user = userServiceManager.readUser(id);
+        if (user != null) {
+            return user.getProjects();
+        } else {
+            return null;
+        }
+
+    }
+
+    @RequestMapping(value = "/adduser", method = RequestMethod.POST)
+    public @ResponseBody
+    String addUser(@RequestParam String userid, @RequestParam String projectId) {
+        String uid = userid;
+        System.out.println("User Id :" + uid);
+        Integer pid = Integer.parseInt(projectId);
+        Project project = projectManager.readProject(pid);
+        System.out.println("User Object :" + userServiceManager.readUser(uid));
+        project.addAssignees(userServiceManager.readUser(uid));
+        projectManager.updateProject(project);
+        return "{\"result\":\"success\"}";
+    }
+
+    @RequestMapping(value = "/removeuser", method = RequestMethod.POST)
+    public @ResponseBody
+    String removeUser(@RequestParam String userid, @RequestParam String projectId) {
+        String uid = userid;
+        Integer pid = Integer.parseInt(projectId);
+        Project project = projectManager.readProject(pid);
+        project.removeAssignees(userServiceManager.readUser(uid));
+        projectManager.updateProject(project);
+        return "{\"result\":\"success\"}";
+    }
+
+    @RequestMapping(value = "/storycount/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    String projectCounts(@PathVariable("id") String id) {
+        Integer pid = Integer.parseInt(id);
+        Project project = projectManager.readProject(pid);
+        Set<Story> stories = (Set<Story>) project.getStories();
+        if (stories != null) {
+            return "{\"result\":" + stories.size() + "}";
+        }
+        return "{\"result\":\"failure\"}";
+
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public @ResponseBody
+    List<Project> createProject(@RequestParam String pTitle, @RequestParam String pDescription,
+                                    @RequestParam String pSprintDuration,
+                                    @RequestParam String pStartDate, @RequestParam String pEndDate,
+                                    @RequestParam String assignees,
+                                    @RequestParam String current_user)
+
+    {
+        Project project = new Project();
+        try {
+
+            project.setTitle(pTitle);
+            project.setDescription(pDescription);
+            Date date = null;
+            Date date1 = null;
+            int sprint_count = 0;
+            int duration = Integer.parseInt(pSprintDuration);
+            try {
+                SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+                if (pStartDate != "" && pEndDate != "") {
+                    date = format.parse(pStartDate);
+                    date1 = format.parse(pEndDate);
+                    project.setStart_date(date);
+                    project.setEnd_date(date1);
+                    sprint_count = getSprintCount(date, date1, Integer.parseInt(pSprintDuration));
+                    project.setNo_of_sprints(sprint_count);
+                } else {
+                    date = format.parse(pStartDate);
+                    project.setStart_date(date);
+                    project.setCurrent_sprint(0);
+                    project.setEnd_date(null);
+                    sprint_count = getSprintCount(date, null, Integer.parseInt(pSprintDuration));
+                    System.out.println("Test: SC: " + sprint_count);
+                    project.setNo_of_sprints(sprint_count);
+                }
+            } catch (ParseException e1) {
+                e1.printStackTrace();
+            }
+            String userStr = assignees;
+            String[] users = userStr.split(",");
+            Set<User> userList = new HashSet<User>();
+            for (String s : users) {
+                System.out.println("user: " + s);
+                User user = userServiceManager.readUser(s);
+                userList.add(user);
+            }
+
+            if (date.after(new Date())) {
+                project.setCurrent_sprint(0);
+                project.setStatus("Not Started");
+            } else {
+                project.setCurrent_sprint(getCurrentSprint(date, date1, duration));
+                project.setStatus("In Progress");
+            }
+
+            project.setAssignees(userList);
+            project.setSprint_duration(duration);
+            project.setCreatedby(current_user);
+            project.setLast_updated(new java.sql.Date(System.currentTimeMillis()));
+            project.setLast_updatedby(current_user);
+            project.setCreation_date(new java.sql.Date(System.currentTimeMillis()));
+
+            projectManager.createProject(project);
+            Date currentdate = date;
+            for (int i = 0; i < sprint_count; i++) {
+
+                Sprint sprint = new Sprint();
+                sprint.setId(i + 1);
+                sprint.setStartdate(new java.sql.Date(currentdate.getTime()));
+                Date enddate = new Date(currentdate.getTime() + ((7 * duration) * 86400000L)
+                                                - 3600000L);
+                if (date1 != null && i == (sprint_count - 1)) {
+                    enddate = new Date(currentdate.getTime() + ((7 * duration) * 86400000L));
+                }
+                if (date1 != null) {
+                    if (enddate.before(date1)) {
+                        sprint.setEnddate(new java.sql.Date(enddate.getTime()));
+                    } else {
+                        sprint.setEnddate(new java.sql.Date(date1.getTime()
+                                                        + (86400000L - 3600000L)));
+                    }
+                } else {
+                    sprint.setEnddate(new java.sql.Date(enddate.getTime()));
+                }
+                if (currentdate.after(new Date())) {
+                    sprint.setStatus("Not Started");
+                } else {
+                    if (enddate.before(new Date())) {
+                        System.out.println(enddate.toString());
+                        sprint.setStatus("Finished");
+                    } else {
+                        System.out.println("Past sprint");
+                        sprint.setStatus("In Progress");
+                        project.setCurrent_sprint(sprint.getId());
+                        project.setStatus("In Progress");
+                        projectManager.updateProject(project);
+                    }
+                }
+                sprint.setProject(project);
+                sprintManager.createSprint(sprint);
+                currentdate = new Date(currentdate.getTime() + ((7 * duration) * 86400000L));
+            }
+
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
             return null;
         }
-       
+
         List<Project> result = new ArrayList<Project>();
         result.add(project);
-		
-	    return result;
-		
-    }
-	
-	@RequestMapping(value="/update", method = RequestMethod.POST)	
-    public @ResponseBody List<Project> updateProject(
-    		@RequestParam String pNo,
-    		@RequestParam String pTitle,
-    		@RequestParam String pDescription,
-    		@RequestParam String pSprintDuration,
-    		@RequestParam String pStartDate,
-    		@RequestParam String pEndDate,
-    		@RequestParam String assignees,
-    		@RequestParam String current_user
-    		)
-	
-	{
-		Project project = projectManager.readProject(Integer.parseInt(pNo));
-		List<Project> result = new ArrayList<Project>();
-		
-		if(project != null){
-			try {
-				
-				project.setTitle(pTitle);
-				project.setDescription(pDescription);
-				project.setLast_updated(new java.sql.Date(System.currentTimeMillis()));
-				project.setLast_updatedby(current_user);
-				
-				projectManager.updateProject(project);
-				
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return null;
-			}
-			result.add(project);
-		}
-       
-		
-	    return result;
-		
-    }
-	
-	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
-	public @ResponseBody String deleteProject(@PathVariable("id") String id) {
-		Project project = projectManager.readProject(Integer.parseInt(id));
-		if(project != null){
-			projectManager.deleteProject(project);
-			System.out.println("SUCCESS");
-			return "{\"result\":\"success\"}";
-		}else{
-			System.out.println("FAILURE");
-			return "{\"result\":\"failure\"}";
-		}
-	}
-	
-	int getSprintCount(Date start, Date end, int duration){
-		if(end != null){
-			int count = (int)(((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24))/(7*duration));
-			int rem = (int)(((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24))%(7*duration));
-			if(rem > 0){
-				return count + 1;
-			}
-			return count;
-		}else{
-			return 1;
-		}
-	}
-	
-	int getCurrentSprint(Date start, Date end, int duration){
-		if(end != null){
-			int count = (int)(((System.currentTimeMillis()- start.getTime()) / (1000 * 60 * 60 * 24))/(7*duration));
-			int rem = (int)(((System.currentTimeMillis() - start.getTime()) / (1000 * 60 * 60 * 24))%(7*duration));
-			if(rem > 0){
-				return count + 1;
-			}
-			return count;
-		}else{
-			return 1;
-		}
-	}
-}
 
+        return result;
+
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public @ResponseBody
+    List<Project> updateProject(@RequestParam String pNo, @RequestParam String pTitle,
+                                    @RequestParam String pDescription,
+                                    @RequestParam String pSprintDuration,
+                                    @RequestParam String pStartDate, @RequestParam String pEndDate,
+                                    @RequestParam String assignees,
+                                    @RequestParam String current_user)
+
+    {
+        Project project = projectManager.readProject(Integer.parseInt(pNo));
+        List<Project> result = new ArrayList<Project>();
+
+        if (project != null) {
+            try {
+
+                project.setTitle(pTitle);
+                project.setDescription(pDescription);
+                project.setLast_updated(new java.sql.Date(System.currentTimeMillis()));
+                project.setLast_updatedby(current_user);
+
+                projectManager.updateProject(project);
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return null;
+            }
+            result.add(project);
+        }
+
+        return result;
+
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    String deleteProject(@PathVariable("id") String id) {
+        Project project = projectManager.readProject(Integer.parseInt(id));
+        if (project != null) {
+            projectManager.deleteProject(project);
+            System.out.println("SUCCESS");
+            return "{\"result\":\"success\"}";
+        } else {
+            System.out.println("FAILURE");
+            return "{\"result\":\"failure\"}";
+        }
+    }
+
+    int getSprintCount(Date start, Date end, int duration) {
+        if (end != null) {
+            int count = (int) (((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24)) / (7 * duration));
+            int rem = (int) (((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24)) % (7 * duration));
+            if (rem > 0) {
+                return count + 1;
+            }
+            return count;
+        } else {
+            return 1;
+        }
+    }
+
+    int getCurrentSprint(Date start, Date end, int duration) {
+        if (end != null) {
+            int count = (int) (((System.currentTimeMillis() - start.getTime()) / (1000 * 60 * 60 * 24)) / (7 * duration));
+            int rem = (int) (((System.currentTimeMillis() - start.getTime()) / (1000 * 60 * 60 * 24)) % (7 * duration));
+            if (rem > 0) {
+                return count + 1;
+            }
+            return count;
+        } else {
+            return 1;
+        }
+    }
+}
